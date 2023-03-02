b8e025e05c541d9ef6cbc62cfcdd4bec769e1b52
JMGbaum <32182454+JMGbaum@users.noreply.github.com>
2023-03-02 15:00:58 -0500

    Fix RecipesController#show * Change `find_by` to `find`

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,6 @@ class RecipesController < ApplicationController
     end
 
     def show
-        @recipe = Recipe.find_by(id: params[:id])
+        @recipe = Recipe.find(id: params[:id])
     end
 end
