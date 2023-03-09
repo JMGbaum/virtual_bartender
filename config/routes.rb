3a42e4f9ab3707d90e197842a04c632ea74444a5
Rebecca Zipper <63117945+beckystar4@users.noreply.github.com>
2023-03-09 18:38:49 -0500

    Basic User Model
    User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # get "/ingredients", to:'ingredients#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
