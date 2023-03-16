e0cf730f28d6635dd9103be7e9ba3747dbac8ec7
JMGbaum <32182454+JMGbaum@users.noreply.github.com>
2023-03-15 22:16:04 -0400

    Update uniqueness validation

--- app/models/ingredient_tag.rb
+++ app/models/ingredient_tag.rb
@@ -1,4 +1,6 @@
 class IngredientTag < ApplicationRecord
     belongs_to :ingredient, inverse_of: :ingredient_tags
     belongs_to :tag, inverse_of: :ingredient_tags
+
+    validates :ingredient_id, uniqueness: { scope: :tag_id }
 end
