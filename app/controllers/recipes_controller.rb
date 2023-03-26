49bcd97e90ecbd0daff66f4077f8a9ec7c6b8da9
JMGbaum <32182454+JMGbaum@users.noreply.github.com>
2023-03-26 15:37:40 -0400

    Paginate recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
     def index
-        @recipes = params[:search].present? ? Recipe.where('title LIKE ? OR description LIKE ? OR instructions LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").select(:id, :title, :description)
-            : Recipe.all.select(:id, :title, :description)
+        @pagy, @recipes = pagy(params[:search].present? ? Recipe.where('title LIKE ? OR description LIKE ? OR instructions LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%").select(:id, :title, :description)
+            : Recipe.all.select(:id, :title, :description))
     end
 
     def show
