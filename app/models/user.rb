21c80c51e3575f435dbbad078d47540e5e87155b
Rebecca Zipper <63117945+beckystar4@users.noreply.github.com>
2023-03-30 15:15:23 -0400

    Inventory add/delete
    Add and delete items from your inventory

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
     has_many :user_recipes, dependent: :destroy, inverse_of: :user
     has_many :recipes, through: :user_recipes, inverse_of: :users
 
+    has_many :ingredient_items, dependent: :destroy, inverse_of: :user
+    has_many :ingredients, through: :ingredient_items, inverse_of: :users
+
     before_save { email.downcase! }
     validates(:first_name, :last_name, presence: true, length: { maximum: 50 })
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
