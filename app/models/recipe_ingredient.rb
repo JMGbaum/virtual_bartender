25ca9766a2f202f49547fac49f6f974f22468fc2
JMGbaum <32182454+JMGbaum@users.noreply.github.com>
2023-04-06 00:04:30 -0400

    Move fraction_symbol into RecipeIngredient model

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -35,12 +35,6 @@ class RecipeIngredient < ApplicationRecord
         "#{amount_as_mixed_number} #{units_abbreviation}"
     end
 
-    def pluralize_units(str)
-        return str if amount < 1
-        
-        str.pluralize(amount)
-    end
-
     # Returns the units abbreviation if there is one
     def units_abbreviation
         case units
@@ -77,5 +71,54 @@ class RecipeIngredient < ApplicationRecord
         return amount.nil? ? custom_unit : pluralize_units(custom_unit) if custom_units?
 
         pluralize_units(units)
+    end
+
+    private
+
+    def pluralize_units(str)
+        return str if amount < 1
+        
+        str.pluralize(amount)
+    end
+
+    def fraction_symbol(num)
+        case(num)
+        when 1/2r
+            '½'
+        when 1/3r
+            '⅓'
+        when 1/4r
+            '¼'
+        when 1/5r
+            '⅕'
+        when 1/6r
+            '⅙'
+        when 1/8r
+            '⅛'
+        when 2/3r
+            '⅔'
+        when 2/5r
+            '⅖'
+        when 3/8r
+            '⅜'
+        when 3/5r
+            '⅗'
+        when 4/5r
+            '⅘'
+        when 5/6r
+            '⅚'
+        when 5/8r
+            '⅝'
+        when 7/8r
+            '⅞'
+        when 1/7r
+            '⅐'
+        when 1/9r
+            '⅑'
+        when 1/10r
+            '⅒'
+        else
+            num.to_s
+        end
     end
 end
