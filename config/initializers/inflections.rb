efb7dc973b962e0b4997929631a78bbdb66091a6
JMGbaum <32182454+JMGbaum@users.noreply.github.com>
2023-04-06 00:07:31 -0400

    Change inflection to irregular

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -8,8 +8,7 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.singular /^(ox)en/i, "\\1"
 #   inflect.irregular "person", "people"
 #   inflect.uncountable %w( fish sheep )
-    inflect.plural 'leaf', 'leaves'
-    inflect.singular 'leaves', 'leaf'
+    inflect.irregular 'leaf', 'leaves'
 end
 
 # These inflection rules are supported but not enabled by default:
