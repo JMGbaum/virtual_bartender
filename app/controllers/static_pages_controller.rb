f52ec21254a74e02ee323106820a70fb32edd676
Rebecca Zipper <63117945+beckystar4@users.noreply.github.com>
2023-05-01 14:22:55 -0400

    Basic implementation
    Still have to work on edge cases

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
+    @recommendation = []
+    if logged_in? 
+      if current_user.user_recipes.where(liked: true).count > 2 && current_user.user_recipes.where(liked: false).count > 2
+        u = current_user
+        Recipe.where.not(id: u.recipes.ids).each do |recipe|
+          break if @recommendation.length() >= 5
+            if u.predict_recipe(recipe) == "recommended"
+              @recommendation.append(recipe)
+            end
+          end 
+      end  
+    end  
   end
 end
