63b4c11002a117cc216ef86ad5d46cf342b8df12
Rebecca Zipper <63117945+beckystar4@users.noreply.github.com>
2023-05-01 14:24:17 -0400

    Update static_pages_controller.rb
    made it more efficient

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,8 @@ class StaticPagesController < ApplicationController
   def home
     @recommendation = []
     if logged_in? 
-      if current_user.user_recipes.where(liked: true).count > 2 && current_user.user_recipes.where(liked: false).count > 2
-        u = current_user
+      u = current_user
+      if u.user_recipes.where(liked: true).count > 2 && u.user_recipes.where(liked: false).count > 2
         Recipe.where.not(id: u.recipes.ids).each do |recipe|
           break if @recommendation.length() >= 5
             if u.predict_recipe(recipe) == "recommended"
