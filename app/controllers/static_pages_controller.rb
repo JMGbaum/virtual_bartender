6ae453ad43aa1bde6b6394a62f24ec244e966a04
Rebecca Zipper <63117945+beckystar4@users.noreply.github.com>
2023-05-02 10:44:40 -0400

    Fixed if statement
    Realized that the homepage would load fine as long as count for both liked and not liked were greater than 1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     @recommendation = []
     if logged_in? 
       u = current_user
-      if u.user_recipes.where(liked: true).count > 2 && u.user_recipes.where(liked: false).count > 2
+      if u.user_recipes.where(liked: true).count > 1 && u.user_recipes.where(liked: false).count > 1
         Recipe.where.not(id: u.recipes.ids).each do |recipe|
           break if @recommendation.length() >= 5
             if u.predict_recipe(recipe) == "recommended"
